525ca4b29dc96a3f6a673d03f336a81ccb2e5a38
Karim-Atef <151665136+Karim-Atef-Mohammed@users.noreply.github.com>
2023-12-25 02:25:04 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,30 @@
+(async () => {
+  let contacts = await fetch("/contactinfo.json");
+  contacts = await contacts.json();
+  const emails = document.querySelectorAll(".email");
+  emails.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      let name = e.target.getAttribute("data-name");
+      window.open(`mailto:${contacts[name].email}`)
+    })
+  });
+})();
+
+$(document).ready(function() {
+    $('.year_2022').click(function() {
+      $('.year_2022').addClass('selected');
+      $('.year_2023').removeClass('selected');
+      $('.board-2023').addClass('hidden'); 
+      $('.board-2022').removeClass('hidden');
+    });
+
+    $('.year_2023').click(function() {
+      $('.year_2023').addClass('selected');
+      $('.year_2022').removeClass('selected');
+      $('.board-2022').addClass('hidden'); 
+      $('.board-2023').removeClass('hidden');
+    });
+  });
+
+
